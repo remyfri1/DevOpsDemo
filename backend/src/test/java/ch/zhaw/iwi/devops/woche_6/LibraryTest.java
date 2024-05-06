6520aaddd69072e14c33e2eef35dfbf28f133d9a
Remy <remy.frigg@gmail.com>
2024-05-06 18:57:44 +0200

    w6_test2_testSearchByTitle_success

--- backend/src/test/java/ch/zhaw/iwi/devops/woche_6/LibraryTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/woche_6/LibraryTest.java
@@ -23,6 +23,7 @@ public class LibraryTest {
     }
 
     @Test
+    //test 1
     public void testAddBookIncreasesCount() {
         assertEquals(2, library.countBooks());
         library.addBook(new Book("Brave New World", "Aldous Huxley"));
@@ -30,11 +31,22 @@ public class LibraryTest {
     }
 
     @Test
+    //test 3
     public void testSearchByTitle() {
         List<Book> results = library.searchByTitle("1984");
         assertFalse(results.isEmpty());
         assertEquals(0, results.size());
         assertEquals(book1, results.get(0));
+    }
+
+
+    @Test
+    //test 2
+    public void testSearchByAuthor() {
+        List<Book> results = library.searchByAuthor("Harper Lee");
+        assertFalse(results.isEmpty());
+        assertEquals(1, results.size());
+        assertEquals(book2, results.get(0));
     }
 
 
